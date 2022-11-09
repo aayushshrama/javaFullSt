71dbf2fc54f3d362c5331b49f1897204860f2b8b
aayushshrama <117735555+aayushshrama@users.noreply.github.com>
2022-11-09 18:48:16 +0530

    hello java

--- test.java
+++ test.java
@@ -1,5 +1,9 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("hello wrold");
+        int a =3;
+        int b=4;
+        int c=a+b;
+        System.out.println(c);
     }
 }
