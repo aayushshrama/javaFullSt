391e129b7ca42c1b697c619d5d2efb2017feef47
aayushshrama <117735555+aayushshrama@users.noreply.github.com>
2022-11-09 19:31:20 +0530

    Coustomerdetails

---
+++ CoustomerDetails.java
@@ -0,0 +1,21 @@
+//task #1
+//IIPRSS3703
+//Aayush sharma
+
+public class CoustomerDetails {
+    public static void main(String[] args) {
+        int coustomerID=101;
+        String coustomerName="jones";
+        String coustomerGender="m";
+        String coustomerEmail="coustomer1@gmail.com";
+        long coustomermobile=1122334455;
+
+        System.out.println("coustomerid--"+coustomerID);
+        System.out.println("coustomerName--"+coustomerName);
+        System.out.println("coustomerGender--"+coustomerGender);
+        System.out.println("coustomerEmail--"+coustomerEmail);
+        System.out.println("coustomermobile--"+coustomermobile);
+
+
+    }
+}
