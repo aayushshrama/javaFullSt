ba0f7fe1ceb49cd30c60b9177615b45f8c936dfe
aayushshrama <117735555+aayushshrama@users.noreply.github.com>
2022-11-10 14:28:22 +0530

    result.java

---
+++ result.java
@@ -0,0 +1,11 @@
+public class result {
+    public static void main(String[] args) {
+        System.out.println(20&15);
+        System.out.println(30&20);
+        System.out.println(80&25);
+        System.out.println(20|15);
+        System.out.println(30|20);
+        System.out.println(80|25);
+
+    }
+}
